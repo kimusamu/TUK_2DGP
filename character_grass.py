a00f2f90b9ae0ef42ee595224d28de4efac29e01
kimusamu <57126265+kimusamu@users.noreply.github.com>
2023-09-21 12:30:51 +0900

    Drill 02 make run to top line
    Drill 02 make run to top line

--- character_grass.py
+++ character_grass.py
@@ -25,13 +25,17 @@ def run_rectangle():
     print('RECTANGLE')
 
     # bottom line
-    for x in range(50, 750 + 1, 5):
-        render_frame(x, 90) # x, y 위치에 캐릭터 그려줄 수 있는
+    #for x in range(50, 750 + 1, 5):
+        #render_frame(x, 90) # x, y 위치에 캐릭터 그려줄 수 있는
+
+    # top line
+    for x in range(750, 50 - 1, - 5):
+        render_frame(x, 550)
     
     pass
 
 while(True):
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
 
