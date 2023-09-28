bde8596b0644d8733475ef97bade419e7950e9c4
kimusamu <57126265+kimusamu@users.noreply.github.com>
2023-09-28 09:57:06 +0900

    make up, down, right, left animation to idle or run
    make up, down, right, left animation to idle or run

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,7 +6,7 @@ tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('drill4_sprite.png')
 
 def handle_events():
-    global running, dir_x, dir_y, idle_or_run
+    global running, dir_x, dir_y, idle_or_run, frame_y, right_left
 
     events = get_events()
 
@@ -17,15 +17,21 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             idle_or_run = True
             if event.key == SDLK_RIGHT:
+                right_left = True
+                frame_y = 200
                 dir_x += 1
 
             elif event.key == SDLK_LEFT:
+                right_left = False
+                frame_y = 200
                 dir_x -= 1
 
             elif event.key == SDLK_UP:
+                frame_y = 0
                 dir_y += 1
 
             elif event.key == SDLK_DOWN:
+                frame_y = 300
                 dir_y -= 1
 
             elif event.key == SDLK_ESCAPE:
@@ -48,6 +54,7 @@ def handle_events():
 
 running = True
 idle_or_run = False
+right_left = True
 
 x = 800 // 2
 y = 90
@@ -56,6 +63,7 @@ dir_y = 0
 
 idle_frame = 0
 run_frame = 3
+frame_y = 300
 
 idle_frame_size = 73
 idle_frame_size_x = 34
@@ -72,20 +80,23 @@ while running:
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
 
     if(idle_or_run == False):
-        character.clip_draw(idle_frame * idle_frame_size + idle_frame_size_x, 0, idle_size_x, idle_size_y, x, y)
+        if(right_left == True):
+            character.clip_draw(idle_frame * idle_frame_size + idle_frame_size_x, frame_y, idle_size_x, idle_size_y, x, y)
+        elif (right_left == False):
+            character.clip_composite_draw(idle_frame * idle_frame_size + idle_frame_size_x, frame_y, idle_size_x, idle_size_y, 0, 'h', x, y, idle_size_x, idle_size_y)
         idle_frame = (idle_frame + 1) % 3
 
     elif(idle_or_run == True):
-        character.clip_draw(run_frame * run_frame_size - run_frame_size_x, 0, run_size_x, run_size_y, x, y)
+        if(right_left == True):
+            character.clip_draw(run_frame * run_frame_size - run_frame_size_x, frame_y, run_size_x, run_size_y, x, y)
+        elif(right_left == False):
+            character.clip_composite_draw(run_frame * run_frame_size - run_frame_size_x, frame_y, run_size_x, run_size_y, 0, 'h', x, y, run_size_x, run_size_y)
         run_frame = (run_frame + 1) % 8
-
         if(run_frame > 6):
             run_frame = 3
 
     update_canvas()
     handle_events()
-
-
 
     x += dir_x * 30
     y += dir_y * 30
