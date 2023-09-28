8c83b33fe01304d2e7e97956d2001fc05b40d34b
kimusamu <57126265+kimusamu@users.noreply.github.com>
2023-09-28 09:51:19 +0900

    make idle and run animation
    make idle and run animation

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,9 +5,8 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('drill4_sprite.png')
 
-
 def handle_events():
-    global running, dir_x, dir_y
+    global running, dir_x, dir_y, idle_or_run
 
     events = get_events()
 
@@ -16,6 +15,7 @@ def handle_events():
             running = False
 
         elif event.type == SDL_KEYDOWN:
+            idle_or_run = True
             if event.key == SDLK_RIGHT:
                 dir_x += 1
 
@@ -32,6 +32,7 @@ def handle_events():
                 running = False
 
         elif event.type == SDL_KEYUP:
+            idle_or_run = False
             if event.key == SDLK_RIGHT:
                 dir_x -= 1
 
@@ -46,22 +47,45 @@ def handle_events():
 
 
 running = True
+idle_or_run = False
+
 x = 800 // 2
 y = 90
-frame = 0
-
 dir_x = 0
 dir_y = 0
+
+idle_frame = 0
+run_frame = 3
+
+idle_frame_size = 73
+idle_frame_size_x = 34
+idle_size_x = 80
+idle_size_y = 90
+
+run_frame_size = 100
+run_frame_size_x = 8
+run_size_x = 96
+run_size_y = 100
 
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 0, 100, 100, x, y)
+
+    if(idle_or_run == False):
+        character.clip_draw(idle_frame * idle_frame_size + idle_frame_size_x, 0, idle_size_x, idle_size_y, x, y)
+        idle_frame = (idle_frame + 1) % 3
+
+    elif(idle_or_run == True):
+        character.clip_draw(run_frame * run_frame_size - run_frame_size_x, 0, run_size_x, run_size_y, x, y)
+        run_frame = (run_frame + 1) % 8
+
+        if(run_frame > 6):
+            run_frame = 3
 
     update_canvas()
     handle_events()
 
-    frame = (frame + 1) % 8
+
 
     x += dir_x * 30
     y += dir_y * 30
