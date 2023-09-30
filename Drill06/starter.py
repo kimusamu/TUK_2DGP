829630e1040da227fb4d6de395db2756bde18c1d
kimusamu <57126265+kimusamu@users.noreply.github.com>
2023-09-30 16:50:30 +0900

    make hand
    make hand

--- Drill06/starter.py
+++ Drill06/starter.py
@@ -5,6 +5,7 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
 
 
 def handle_events():
@@ -30,7 +31,8 @@ hide_cursor()
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.draw(x, y)
+    #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
 
