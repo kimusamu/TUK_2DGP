31be014f0a1ce297bda3285ed91c189ddf2e7f62
kimusamu <57126265+kimusamu@users.noreply.github.com>
2023-09-30 20:50:30 +0900

    follow char to hand
    follow char to hand

--- Drill06/starter.py
+++ Drill06/starter.py
@@ -6,67 +6,63 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
-follow_hand = load_image('hand_arrow.png')
-
-def save_hand_xy():
-    global hand_x, hand_y, list_x, list_y
-    list_x.append(hand_x)
-    list_y.append(hand_y)
 
 def handle_events():
     global running
-    global x, y, hand_x, hand_y
+    global x, y, hand_x, hand_y, mx, my
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+            mx, my = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            hand_x, hand_y = event.x, TUK_HEIGHT - 1 - event.y
-            save_hand_xy()
-
-def character_move():
-    global list_x, list_y, char_x, char_y
-    if len(list_x) > 0:
-        for i in range(0, 100 + 1, 1):
-            t = i / 100
-            char_x = (1 - t) * char_x + t * list_x[0]
-            char_y = (1 - t) * char_y + t * list_y[0]
-            draw_char()
-
-        if(char_x == list_x[0] and char_y == list_y[0]):
-            del list_x[0], list_y[0]
-
-def draw_char():
-    global frame
-    character.clip_draw(frame * 100, 0, 100, 100, char_x, char_y, 100, 100)
-    frame = (frame + 1) % 8
+            list_x.append(event.x)
+            list_y.append(TUK_HEIGHT - 1 - event.y)
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+mx, my = TUK_WIDTH // 2, TUK_HEIGHT // 2
+go_x, go_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 hand_x = 0
 hand_y = 0
-char_x = TUK_WIDTH // 2
-char_y = TUK_HEIGHT // 2
+
 hide_cursor()
 
 list_x = []
 list_y = []
 
 while running:
-    clear_canvas()
-    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    hand.draw(x, y)
-
-    for i in range (len(list_x)):
-        follow_hand.draw(list_x[i], list_y[i])
-
-    character_move()
-    draw_char()
-
-    update_canvas()
     handle_events()
+
+    if len(list_x) > 0:
+        for i in range(1, 100 + 1, 1):
+            clear_canvas()
+            TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+            handle_events()
+            t = i / 100
+            x = (1 - t) * go_x + t * list_x[0]
+            y = (1 - t) * go_y + t * list_y[0]
+            character.clip_draw(frame * 100, 0, 100, 100, x, y, 100, 100)
+            frame = (frame + 1) % 8
+            hand.draw(mx, my)
+            for i in range(len(list_x)):
+                hand.draw(list_x[i], list_y[i])
+            update_canvas()
+            delay(0.01)
+
+        if (x == list_x[0] and y == list_y[0]):
+            del list_x[0], list_y[0]
+            go_x, go_y = x, y
+
+    else:
+        clear_canvas()
+        TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+        hand.draw(mx, my)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y, 100, 100)
+        frame = (frame + 1) % 8
+        update_canvas()
+        delay(0.01)
 
 close_canvas()
